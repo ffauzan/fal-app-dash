d940ed8693c0d8e6066bfd12225b3abc95c5cb5d
Ffauzan <f.fauzan@mail.ugm.ac.id>
2022-05-18 15:40:12 +0700

    add app list component

--- components/Layout.js
+++ components/Layout.js
@@ -3,12 +3,12 @@ import Head from 'next/head'
 
 function Layout({ title, children }) {
   return (
-    <div className='bg-blue-300'>
+    <div className="bg-grey-300">
         <Head>
             <title>{title}</title>
             <link rel="icon" href="/favicon.ico" />
         </Head>
-        <main className='container mx-auto max-w-xl pt-6 min-h-screen'>
+        <main className="mx-auto max-w-screen-xl p-2 pt-6 min-h-screen">
             {children}
         </main>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,61 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
+import AppList from '../components/AppList'
 import Layout from '../components/Layout'
 
-export default function Home() {
+
+export default function Home(props) {
+  // console.log(props.myApps[1])
   return (
     <Layout title="Fal's Apps">
       <h1 className='text-4xl text-center'>Fal&#39;s Apps</h1>
+      <AppList myApps={props.myApps}></AppList>
+    
     </Layout>
   )
+}
+
+export function getStaticProps(context) {
+  const myApps = [
+    {
+      name: 'Shy Mail',
+      desc: 'A temporary dispossable email',
+      url: 'https://shy.my.id',
+      githubLink: 'https://github.com',
+      imgUrl: 'https://is3.cloudhost.id/fimg/lgm.png'
+    },
+    {
+      name: 'Oof QR',
+      desc: 'URL shortener & dynamic link QR code',
+      url: 'https://oof.my.id',
+      githubLink: 'https://github.com',
+      imgUrl: 'https://is3.cloudhost.id/fimg/pp.jpg'
+    },
+    {
+      name: 'Oof QR',
+      desc: 'URL shortener & dynamic link QR code',
+      url: 'https://oof.my.id',
+      githubLink: 'https://github.com',
+      imgUrl: 'https://is3.cloudhost.id/fimg/sn.jpg'
+    },
+    {
+      name: 'Oof QR',
+      desc: 'URL shortener & dynamic link QR code',
+      url: 'https://oof.my.id',
+      githubLink: 'https://github.com',
+      imgUrl: 'https://is3.cloudhost.id/fimg/lgm.png'
+    },
+    {
+      name: 'Oof QR',
+      desc: 'URL shortener & dynamic link QR code',
+      url: 'https://oof.my.id',
+      githubLink: 'https://github.com',
+      imgUrl: 'https://is3.cloudhost.id/fimg/pp.jpg'
+    },
+  ]
+
+  return {
+    props: {myApps}
+  }
 }
