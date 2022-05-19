b14352e76c663ed02b80594d6a2cc3a0b8d69cae
Ffauzan <f.fauzan@mail.ugm.ac.id>
2022-05-19 11:16:32 +0700

    some css stuff

--- components/Header.js
+++ components/Header.js
@@ -9,18 +9,19 @@ function Headers2() {
             Fal&#39;s Apps
           </h1>
         </div>
-        <nav className="my-auto flex content-center space-x-2 md:justify-around md:space-x-1">
-          <a href="https://fal.my.id">
-            <button className="rounded-l-full border border-slate-100 py-1 px-2 text-lg font-medium text-gray-100 md:py-2 md:px-3">
+        <div className="flex my-auto justify-center md:justify-around md:space-x-1 space-x-2">
+          <a className="" href="https://fal.my.id">
+            <button className="py-1 px-2 text-lg font-medium text-gray-100 md:py-2 md:px-3 hover:underline">
               Home
             </button>
           </a>
-          <a target="_blank" rel="noreferrer" href="https://github.com/ffauzan">
-            <button className="rounded-r-full border-2 border-slate-100 py-1 px-2 text-lg font-medium text-gray-100 md:py-2 md:px-3">
+          <a className="" target="_blank" rel="noreferrer" href="https://github.com/ffauzan">
+            <button className="py-1 px-2 text-lg font-medium text-gray-100 md:py-2 md:px-3 hover:underline">
               Github
             </button>
+            {/* <h2 className="font-semibold text-lg text-gray-100 hover:underline">Github</h2> */}
           </a>
-        </nav>
+        </div>
       </div>
     </>
   );
